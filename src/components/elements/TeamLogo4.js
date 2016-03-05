c477823048e04dd52bda39681b77f14319726489
bharat441984 <bharat441984@gmail.com>
2016-03-05 12:32:29 +0100

    Add new Logo, JS and CSS

---
+++ src/components/elements/TeamLogo4.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './TeamLogo.scss'
+
+export default class TeamLogo4 extends Component {
+
+  render() {
+    return (
+      <div>
+        <span className={styles.logo}></span>
+        <h2>SunBlocker</h2>
+      </div>
+    )
+  }
+}
