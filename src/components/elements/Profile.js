f6e1f8b02a49e0933442d95f1260fd8351ac110f
Alexander Holbreich <alexander@holbreich.org>
2016-03-05 14:08:52 +0100

    enabled name

--- src/components/elements/Profile.js
+++ src/components/elements/Profile.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import styles from './Profile.scss'
 import { IndexLink } from 'react-router'
 
+@connect(
+  state => ({
+    username: state.sendbird.user_name
+  })
+)
 export default class Profile extends React.Component {
 
   render() {
@@ -9,6 +15,7 @@ export default class Profile extends React.Component {
       <nav className="text-center">
         <div className={styles.userContainer}>
           <span className={styles.user}>
+            {this.props.username}
             <IndexLink to="/" className={styles.signout} />
           </span>
         </div>
